1d3e48c9696c09a0aa9f8d3ecf497a3295cce4b8
joaovsa7 <jvsadesign@gmail.com>
2020-04-13 19:03:47 -0300

    first commit, creating the project and do initial config

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Text('Flutter Demo Home Page'),
+    );
+  }
+}
